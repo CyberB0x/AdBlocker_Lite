498307cea6c1d9c645f8114f7b5cb4e47bb839a2
Arslon <arslonbekefsun09@gmail.com>
2025-05-18 18:30:30 +0300

    AdBlocker

--- background.js
+++ background.js
@@ -1,20 +1,21 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set({ enabled: true });
+    chrome.storage.local.set({ enabled: true });
 });
 
 chrome.action.onClicked.addListener((tab) => {
-  chrome.storage.local.get("enabled", ({ enabled }) => {
-    const newState = !enabled;
+   chrome.storage.local.get("enabled",({ enabled }) =>{
+      const newState = !enabled;
 
-    chrome.storage.local.set({ enabled: newState }, () => {
-      chrome.declarativeNetRequest.updateEnabledRulesets({
-        enableRulesetIds: newState ? ["block_ads"] : [],
-        disableRulesetIds: newState ? [] : ["block_ads"]
+      chrome.storage.local.set({enabled: newState}, () => {
+          chrome.declarativeNetRequest.updateEnabledRequests({
+
+             enabledRulesIds: newState ? ["block_ads"] : [],
+             disabledRulesIds: newState ? [] : ["block_ads"]
+          });
+
+          if (tab.id) {
+             chrome.tabs.reload(tab.id);
+          }
       });
-
-      if (tab.id) {
-        chrome.tabs.reload(tab.id);
-      }
-    });
-  });
+   });
 });
